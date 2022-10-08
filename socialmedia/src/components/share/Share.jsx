07316244e081bbff184aac4aa9a8558bdcbacee1
azaa <doogii1145@gmail.com>
2022-10-08 14:05:16 +0800

    a

---
+++ socialmedia/src/components/share/Share.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Share() {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+      <div className="shareTop">
+        <img className="shareProfileImg" src="/assets/person/1.png" alt="" />
+          <input 
+            placeholder="What's your in mind Hannah?" 
+            className="shareInput"/>
+      </div>
+      </div>
+    </div>
+  )
+}
