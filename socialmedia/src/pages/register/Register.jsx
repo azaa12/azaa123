40443e885bb7eb790f4b312c9535f124f84600d6
azaa <doogii1145@gmail.com>
2022-10-08 15:26:38 +0800

    g

--- socialmedia/src/pages/register/Register.jsx
+++ socialmedia/src/pages/register/Register.jsx
@@ -2,6 +2,26 @@ import "./register.css"
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="login">
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">Azjargalsocial</h3>
+                <span className="loginDesc">
+                Connect with friends and the world around you on Narangoosocial.
+                </span>
+            </div>
+            <div className="loginRight"></div>
+                <div className="loginBox">
+                  <input placeholder="UserName" className="loginInput" />
+                  <input placeholder="Email" className="loginInput" />
+                  <input placeholder="Password" className="loginInput" />
+                  <input placeholder="Password Again" className="loginInput" />
+                  <button className="loginButton">Sign Up</button>
+                  <button className="loginRegisterButton">
+                    Log into Account
+                  </button>
+                </div>
+        </div>
+    </div>
   )
 }
