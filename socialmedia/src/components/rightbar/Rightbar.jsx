3bd8c65e653643767b0c18a3c98514e85dff8f8d
azaa <doogii1145@gmail.com>
2022-10-08 13:26:27 +0800

    aza

--- socialmedia/src/components/rightbar/Rightbar.jsx
+++ socialmedia/src/components/rightbar/Rightbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import "./rightbar.css"
 
 export default function Rightbar() {
   return (
--- socialmedia/src/components/topbar/Topbar.jsx
+++ socialmedia/src/components/topbar/Topbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import "./topbar.css"
 
 export default function Topbar() {
   return (
