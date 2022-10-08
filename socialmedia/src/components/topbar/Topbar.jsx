a59303be9eb3163d7c30294687860ae7c2df50a9
azaa <doogii1145@gmail.com>
2022-10-08 13:29:12 +0800

    a

--- socialmedia/src/components/topbar/Topbar.jsx
+++ socialmedia/src/components/topbar/Topbar.jsx
@@ -2,6 +2,23 @@ import "./topbar.css"
 
 export default function Topbar() {
   return (
-    <div>Topbar</div>
+    <div  className="topbarContainer">
+            <div className="topbarLeft">
+              <span className="logo">Azaasocial</span>
+            </div>
+            <div className="topbarCenter">
+              <div className="searchbar">
+                <Search className="searchIcon"/>
+                <input 
+                  placeholder="Search for friend, post or video" 
+                  className="searchInput"
+                />
+              </div>
+            </div>
+            <div className="topbarRight">
+              <div className="topbarLinks">
+                <span className="topbarLinks">Homepage</span>
+                <span className="topbarLinks">Timeline</span>
+              </div>  
   )
 }
