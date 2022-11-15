5c0265090395b4cac6afad4bec13d922a66a4292
zndra <doogii1145@gmail.com>
2022-11-15 14:41:37 +0800

    1

--- socialmedia/src/App.js
+++ socialmedia/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
 
 
+
 function App() {
   return <Home />
 }
--- socialmedia/src/pages/home/Home.jsx
+++ socialmedia/src/pages/home/Home.jsx
@@ -1,18 +1,31 @@
-import "./home.css"
-import Rightbar from '../../components/rightbar/Rightbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Topbar from '../../components/topbar/Topbar'
-import Feed from "../../components/feed/Feed"
+import "./home.css";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topbar from "../../components/topbar/Topbar";
+import Feed from "../../components/feed/Feed";
+import { useEffect,useState } from "react";
+import { sendRequest,urlLookup } from "../../settings/settings"
 
 export default function Home() {
+  const [datas, setDatas] = useState();
+  useEffect(() => {
+    const bodyChiglel = {
+      action: "chiglel",
+    };
+
+    sendRequest(urlLookup, bodyChiglel).then((data) => setDatas(data));
+  },[]);
+
+ 
   return (
     <>
-    <Topbar />
-    <div className="homeContainer">
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-    </div>
-    </> 
-  )
+    <p>{datas && JSON.stringify(datas)}</p>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
